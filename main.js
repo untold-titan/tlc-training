64a3a016a9f41de8372bff35dfe3f8c82987afa8
Titan <untoldtitan3k@gmail.com>
2022-12-15 20:06:58 -0700

    Switched to Vanilla JS

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('#app').innerHTML = `
+  <div>
+  </div>
+`
+//In a normal web application that is built in vanilla JS, and the pages are injected into the html programatically. This is what ReactJS does.
+//If we want more functionality out of the site, this will be where it goes, or we switch to a JS framework, like Svelte
---
+++ public/README.md
@@ -0,0 +1,2 @@
+# Public Folder
+Anything in here will be automatically hosted by vite, the build tool.
